9beef1e3b81ade4f8fba3b576d6167097c961d08
dorntrevor7 <dorntrevor7@gmail.com>
2022-09-15 17:38:17 -0700

    Added first two questions

--- script.js
+++ script.js
@@ -7,7 +7,23 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
+function generatePassword() {
+  // characteristcs
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const number = "0123456789";
+  const special = "!@#$&*-";
+
+  // length question that stores the user's desired length"
+  const passLength = prompt("Enter your desired Password length");
+  console.log(passLength);
+
+  // confirms whether or not the password contains Capital letters
+  const capsQuestion = confirm(
+    "Do you want to have Capital letters in your password?"
+  );
+  console.log(capsQuestion);
 }
 
 // Add event listener to generate button
