a9d8edec7392309c2734f7e26b4a787b7af0e5a1
dorntrevor7 <dorntrevor7@gmail.com>
2022-09-15 17:50:26 -0700

    for loop condition on all caps complete

--- script.js
+++ script.js
@@ -14,32 +14,38 @@ function generatePassword() {
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
   const number = "0123456789";
   const special = "!@#$&*-";
+  let password = "";
 
   // length question that stores the user's desired length"
   const passLength = prompt("Enter your desired Password length");
-  console.log(passLength);
+  console.log(passLength); // Users entry
 
   // confirms whether or not the password contains Capital letters
   const capsQuestion = confirm(
     "Do you want to have Capital letters in your password?"
-  );
-  console.log(capsQuestion);
+  ); // console.log(capsQuestion); // Boolean
 
   // confirms whether or not the password contains Capital letters
   const lowQuestion = confirm(
     "Do you want to have Lowercase letters in your password?"
-  );
-  console.log(capsQuestion);
+  ); // console.log(capsQuestion); // Boolean
 
   // confirms whether or not the password contains Capital letters
   const specQuestion = confirm(
     "Do you want to have Special Characters in your password?"
-  );
-  console.log(specQuestion);
+  ); // console.log(specQuestion); // Boolean
 
   // confirms whether or not the password contains Capital letters
   const numQuestion = confirm("Do you want to have Numbers in your password?");
-  console.log(numQuestion);
+  // console.log(numQuestion);// Boolean
+
+  for (let i = 0; i < parseInt(passLength); i++) {
+    if (capsQuestion) {
+      password += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+      console.log(password);
+    }
+  }
+  return password;
 }
 
 // Add event listener to generate button
